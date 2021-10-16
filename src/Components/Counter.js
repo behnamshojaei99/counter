bf9c30eae2966c506728697edb604541e8a30cfb
behnamShojaei <behnamshojaei77@gmail.com>
2021-10-16 12:20:50 +0330

    Counter with useReducer

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,24 +1,40 @@
-import React, {useState} from 'react';
+import React, {useState, useReducer} from 'react';
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case "INCREMENT":
+            return {...state, counter: state.counter + 1};
+            break;
+        case "DECREMENT":
+            return {...state, counter: state.counter - 1};
+            break;
+        case "NEUTRAL":
+            return {...state, counter: 0};
+            break;
+        default: 
+            return state;
+    }
+}
 
 const Counter = ( props ) => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {counter: 0});
+
     const incrementHandler = () => {
-        setCounter(counter + 1);
+        dispatch({type: "INCREMENT"})
     }
 
     const decrementHandler = () => {
-        setCounter(counter - 1);
+        dispatch({type: "DECREMENT"})
     }
-
     const neutralHandler = () => {
-        setCounter(0);
+        dispatch({type: "NEUTRAL"})
     }
 
     return (
-        <div className='row justify-content-center align-items-center'>
+        <div className='row justify-content-center align-items-center h-100'>
             <div className='col-md-5' style={{border: '5px solid black'}}>
                 <h1 className='text-center m-3 fw-bold'>Counter</h1>
-                <h2 className='text-center m-3 fw-bold'>{ counter }</h2>
+                <h2 className='text-center m-3 fw-bold'>{ state.counter }</h2>
                 <button onClick={incrementHandler} className='btn btn-success m-3'>Increment</button>
                 <button onClick={decrementHandler} className='btn btn-danger m-3'>Decrement</button>
                 <button onClick={neutralHandler} className='btn btn-secondary m-3'>Neutral</button>
